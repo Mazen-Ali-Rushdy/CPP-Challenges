9d9b5f4356d9fd359fd00d51c04b14459ed21213
mazen <mazen.ali.rushdy@gmail.com>
2025-08-29 22:58:41 +0300

    Convert a Boolean to a String

--- app.cpp
+++ app.cpp
@@ -3,19 +3,17 @@
 #include <vector>
 using namespace std;
 
-
-int arrayPlusArray(std::vector<int> a, std::vector<int> b) {
-  int result = 0;
-  for (int i : a)
-    result += i;
-  for (int i : b)
-    result += i;
+std::string boolean_to_string(bool b){
+  //Your code here
+  string result;
+  if (b == 0)
+    result = "false";
+  else
+    result = "true";
   return result;
 }
 
 int main () {
-  vector <int> arr1 {1, 2, 4};
-  vector <int> arr2 {2, 2, 4};
-  cout << arrayPlusArray(arr1, arr2);
+  cout << boolean_to_string(true);
   return 0;
 }
