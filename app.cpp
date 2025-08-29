568ef89031d74b575e7d6816f059e9f09fafe317
mazen <mazen.ali.rushdy@gmail.com>
2025-08-29 22:56:52 +0300

    Array plus array

--- app.cpp
+++ app.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-std::string removeExclamationMarks(std::string str){
-  //your code here
-  std::string clean = "";
-  for (int i = 0; i < str.length(); i++)
-  {
-    if (str[i] != '!')
-    {
-      clean += str[i];
-    }
-  }
-  return clean;
-  
+
+int arrayPlusArray(std::vector<int> a, std::vector<int> b) {
+  int result = 0;
+  for (int i : a)
+    result += i;
+  for (int i : b)
+    result += i;
+  return result;
 }
 
 int main () {
-  cout << removeExclamationMarks("Hello World!");
+  vector <int> arr1 {1, 2, 4};
+  vector <int> arr2 {2, 2, 4};
+  cout << arrayPlusArray(arr1, arr2);
   return 0;
 }
