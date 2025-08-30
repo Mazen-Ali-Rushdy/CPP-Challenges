93c37255a3923c8defdce7a3db0d395d6f7bc076
mazen <mazen.ali.rushdy@gmail.com>
2025-08-30 03:32:57 +0300

    String ends with?

--- app.cpp
+++ app.cpp
@@ -6,19 +6,24 @@
 using namespace std;
 
 
-std::string people_with_age_drink(int age) {
-  if (age < 14)
-    return "drink toddy";
-  else if (age < 18) 
-    return "drink coke";
-  else if (age < 21)
-    return "drink beer";
-  else
-    return "drink whisky";
+bool solution(std::string const &str, std::string const &ending) {
+  int str_idx = str.length() - 1;
+  int ending_idx = ending.length() - 1;
+  int result = 0;
+  
+  if (ending.length() > str.length()) return false;
+  
+  for (int i = 0; i < ending.length(); i++) {
+    if (str[str_idx - i] != ending[ending_idx - i]) {
+      return false;
+    }
+  }
+  
+  return true;
+  
 }
 
-
 int main () {
-  cout << people_with_age_drink(15);
+  cout << solution("abcde", "cde");
   return 0;
 }
