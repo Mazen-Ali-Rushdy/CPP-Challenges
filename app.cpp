ea4b2699362a28b1644c22fbee2a7f565c505ac6
mazen <mazen.ali.rushdy@gmail.com>
2025-08-30 05:02:08 +0300

    String repeat

--- app.cpp
+++ app.cpp
@@ -5,32 +5,15 @@
 #include <array>
 using namespace std;
 
-
-std::vector<int> evenNumbers(std::vector<int> arr, int n) {
-  vector <int> new_vec(n);
-  int last_idx = n - 1;
-  if (n == 0) {
-    return {};
+std::string repeat_str(size_t repeat, const std::string& str) {
+  string result;
+  for (int i = 0; i < repeat; i++) {
+    result += str;
   }
-  for (int i = arr.size() - 1; i >= 0; i--) {
-    if (arr[i] % 2 == 0) {
-      new_vec.at((last_idx)--) = arr[i];
-      if (last_idx < 0) {
-        break;
-      }
-    }
-  }
-  
-  return new_vec;
+  return result;
 }
 
 int main () {
-  vector <int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  vector <int> result = evenNumbers(nums, 3);
-
-  for (int num : result) {
-    cout << num << "\t";
-  }
-  
+  cout << repeat_str(17, "*");
   return 0;
 }
